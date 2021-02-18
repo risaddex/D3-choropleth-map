338fca1329a1983f885544c5b618deaec4c66152
risaddex <risaddex@gmail.com>
2021-02-18 14:29:54 -0300

    8/12 tests passing

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 const WIDTH = window.screen.availWidth * .85
 const HEIGHT = window.screen.availHeight * .85
 const PADDING = 60;
-const EDUCATION_DATA =
-  'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json';
-const COUNTY_DATA =
-  'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';
+const EDUCATION_DATA = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json';
+const COUNTY_DATA = 'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json';
 
 //! SVG
 const svg = d3.select('.chart-container')
@@ -14,8 +12,7 @@ const svg = d3.select('.chart-container')
   .attr('height', HEIGHT);
 
 const path = d3.geoPath();
-  
-
+let edu, county, filt;
 //! DATA SCALE
 
 // Load external data and boot
@@ -26,22 +23,24 @@ d3.queue()
 
 function ready(error, usa, data) {
   if(error) throw new Error(error)
-  const bachelor =  data.map((d, i) => d.bachelorsOrHigher)
+  const bachelor = data.map((d, i) => d.bachelorsOrHigher);
 
+  edu = data
+  county = topojson.feature(usa, usa.objects.counties).features
+  
   //! CONSTS
   const max = d3.max(bachelor)
   const min = d3.min(bachelor) 
 
-  console.log(usa, data)
   // !COLOR SCALE
   const colors = d3.scaleThreshold()
     .domain(d3.range(min, max, (max - min)/8))
     .range(d3.schemeBlues[9]);
-
+  // filt = colors
   // const colorScale = d3.scaleLinear()
   //   .domain(colors.range())
   //   .range(bachelor);
-console.log(data, usa)
+
   // Draw the map
   svg.append("g")
     .attr('class', "counties")
@@ -49,13 +48,20 @@ console.log(data, usa)
     .data(topojson.feature(usa, usa.objects.counties).features)
     .enter()
     .append("path")
-    // draw each state
+    // draw each state  
     .attr('class', 'county')
     .attr("d", d3.geoPath())
-    .attr('data-fips', (d) => d)
+    .attr('data-fips', (d) => d.id)
+    .attr('data-education', (d) => {
+      let result = edu.filter((obj) => obj.fips === d.id)
+
+      return result[0] ? result[0].bachelorsOrHigher : 0;
+    })
     // set the color of each country
-    .attr("fill", (countie, i) => {
-      return colors(bachelor[i])
+    .attr("fill", (county, i) => {
+      let result = data.filter((obj) => obj.fips === county.id);
+
+      return result[0] ? colors(result[0].bachelorsOrHigher) : colors(0) //handle undefined
     });
 
   svg.append('path')
