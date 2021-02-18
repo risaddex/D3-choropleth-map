4c5024bfc935d48b8c3efb904d79fc6cde03bc34
risaddex <risaddex@gmail.com>
2021-02-18 07:40:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//! CONSTS
+const WIDTH = window.screen.availWidth * .85
+const HEIGHT = window.screen.availHeight * .85
+const PADDING = 60;
+
+//! SVG
+const svg =
+  d3.select('.chart-container')
+    .append('svg')
+    .attr('width', WIDTH)
+    .attr('height', HEIGHT);
+    
+
+
+
+
+    
